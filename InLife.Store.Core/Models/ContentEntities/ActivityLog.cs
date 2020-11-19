0bea0bff1184383d61e0b13e184f0aa5fa0b6cd6
Glen Reyes <glen@projectgrey.net>
2020-11-19 10:21:08 +0800

    Fixed the User Activity Logging in CMS.

--- InLife.Store.Core/Models/ContentEntities/ActivityLog.cs
+++ InLife.Store.Core/Models/ContentEntities/ActivityLog.cs
@@ -5,12 +5,20 @@ namespace InLife.Store.Core.Models
 {
 	public class ActivityLog : Entity<int>
 	{
-		public string ActionPerfomed { get; set; }
-		public string ActivityDescription { get; set; }
-		public string IpAddress { get; set; }
-		public DateTimeOffset? ActivityDate { get; set; }
+		//public string ActionPerfomed { get; set; }
+		//public string ActivityDescription { get; set; }
+		//public string IpAddress { get; set; }
+		//public DateTimeOffset? ActivityDate { get; set; }
 
-		public string ActivityById { get; set; }
-		public virtual User ActivityBy { get; set; }
+		//public string ActivityById { get; set; }
+		//public virtual User ActivityBy { get; set; }
+
+		public string Action { get; set; }
+		public string Description { get; set; }
+
+		public virtual User TransactionBy { get; set; }
+		public string TransactionByName { get; set; }
+		public DateTimeOffset? TransactionDate { get; set; }
+		public string TransactionRemoteAddress { get; set; }
 	}
 }
